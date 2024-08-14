e0c7bb53af997a6d7054d7179bb264a42dcfb34b
markconnelly <mark.connelly@adactus.co.uk>
2024-08-14 09:33:10 +0200

    added in unit tests for vm

---
+++ LotteryApp.Core/Services/ILotteryDataService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LotteryApp.Core.Models;
+
+namespace LotteryApp.Core.Services
+{
+    public interface ILotteryDataService
+    {
+        Task<List<LotteryDrawModel>> GetLotteryDrawsAsync();
+    }
+}
