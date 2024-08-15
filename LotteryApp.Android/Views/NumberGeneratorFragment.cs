840b032ae2c13a2be96dc61cd57e80b31210307a
markconnelly <mark.connelly@adactus.co.uk>
2024-08-15 08:41:17 +0200

    updating winner/loser text

--- LotteryApp.Android/Views/NumberGeneratorFragment.cs
+++ LotteryApp.Android/Views/NumberGeneratorFragment.cs
@@ -114,6 +114,7 @@ namespace LotteryApp.Android
             var isWinner = _viewModel.LotteryDraws.Any(draw => LotteryNumberComparer.AreNumbersMatching(randomNumbers, draw));
 
             resultTextView.Text = isWinner ? "Winner!" : "No wins today";
+            resultTextView.SetTextColor(isWinner ? Context.GetColorStateList(Resource.Color.winnerTextColor) : Context.GetColorStateList(Resource.Color.loserTextColor));
         }
     }
 }
